1a56ddf250b3f376e98b1cbb13d9f11d667266d9
jaime hritsik <jhritsik@gmail.com>
2015-07-23 10:56:00 -0400

    slideshow

--- app/assets/js/project.js
+++ app/assets/js/project.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
 	$('#fullpage').fullpage({
-				anchors: ['firstPage'],
-				sectionsColor: ['#4A6FB1', '#939FAA', '#323539'],
 				fitToSection:false,
 				autoScrolling: false,
 				slidesNavigation:true
